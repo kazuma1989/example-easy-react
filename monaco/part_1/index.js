012035d9773b54bdcfb825e1d7d385fcc8060b30
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-11 23:08:59 +0900

    part 1: API から取得するだけ

--- monaco/part_1/index.js
+++ monaco/part_1/index.js
@@ -1,17 +1,36 @@
 // @ts-check
 
-import {
-  html,
-  render,
-  useState,
-  useEffect,
-  // @ts-ignore
-} from "https://cdn.pika.dev/htm/preact/standalone.module.js";
-// @ts-ignore
-import { css, cx, injectGlobal } from "https://cdn.pika.dev/emotion";
+document.body.innerHTML = `
+  <h1>TODO list (vanilla)</h1>
 
-function App() {
-  return html`<h1>Hello</h1>`;
+  <div id="list"></div>
+`;
+
+/** @type {HTMLElement} */
+const todoList = document.body.querySelector("#list");
+
+// API からデータ取得
+fetch("./db.json")
+  .then((r) => r.json())
+  .then(({ todos }) => {
+    todos.reverse().forEach((todo) => {
+      prependItem(todoList, todo);
+    });
+  });
+
+/**
+ * @param {HTMLElement} list
+ * @param {object}  todo
+ * @param {boolean} todo.done
+ * @param {string}  todo.text
+ */
+function prependItem(list, { text, done }) {
+  const item = document.createElement("label");
+  item.innerHTML = `<input type="checkbox" /> ${text}`;
+
+  /** @type {HTMLInputElement} */
+  const checkbox = item.querySelector("input[type=checkbox]");
+  checkbox.checked = done;
+
+  list.prepend(item);
 }
-
-render(html`<${App} />`, document.body);
