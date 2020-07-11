0f807e97ceaa5c7c19a53a306eba136c573e23ce
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-11 23:16:48 +0900

    part 2: onChange で見た目切り替え
    part 0: blank も追加

--- monaco/part_2/index.js
+++ monaco/part_2/index.js
@@ -1,17 +1,55 @@
 // @ts-check
 
-import {
-  html,
-  render,
-  useState,
-  useEffect,
-  // @ts-ignore
-} from "https://cdn.pika.dev/htm/preact/standalone.module.js";
 // @ts-ignore
-import { css, cx, injectGlobal } from "https://cdn.pika.dev/emotion";
+import { css, cx } from "https://cdn.pika.dev/emotion";
 
-function App() {
-  return html`<h1>HELLO</h1>`;
+document.body.innerHTML = `
+  <h1>TODO list (vanilla)</h1>
+
+  <div id="list"></div>
+`;
+
+/** @type {HTMLElement} */
+const todoList = document.body.querySelector("#list");
+
+// API からデータ取得
+fetch("./db.json")
+  .then((r) => r.json())
+  .then(({ todos }) => {
+    todos.reverse().forEach((todo) => {
+      prependItem(todoList, todo);
+    });
+  });
+
+/**
+ * @param {HTMLElement} list
+ * @param {object}  todo
+ * @param {boolean} todo.done
+ * @param {string}  todo.text
+ */
+function prependItem(list, { text, done }) {
+  const item = document.createElement("label");
+  item.innerHTML = `<input type="checkbox" /> ${text}`;
+
+  /** @type {HTMLInputElement} */
+  const checkbox = item.querySelector("input[type=checkbox]");
+  checkbox.checked = done;
+
+  const onChange = () => {
+    item.className = cx(
+      css`
+        font-weight: normal;
+        white-space: pre-wrap;
+      `,
+      checkbox.checked &&
+        css`
+          text-decoration: line-through;
+          opacity: 0.5;
+        `
+    );
+  };
+  checkbox.addEventListener("change", onChange);
+  onChange();
+
+  list.prepend(item);
 }
-
-render(html`<${App} />`, document.body);
